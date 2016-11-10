e5273217569ad697f1c4c0a7da5f3378af24fc6d
genesis <genesis@DESKTOP-4TIND3N>
2016-11-10 15:53:12 +0800

    General architecture

--- DoDo.io/src/tw/edu/ncu/softwareengineering/dodoio/Client.java
+++ DoDo.io/src/tw/edu/ncu/softwareengineering/dodoio/Client.java
@@ -1,6 +1,62 @@
 package tw.edu.ncu.softwareengineering.dodoio;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class Client
 {
-
+	Socket socket;
+	static DataInputStream rdata;
+	static DataOutputStream wdata;
+	static int userID;
+	
+	public Client()
+	{
+		/*
+		 * Client constructor
+		 * make the socket connection
+		 * and handle the socket exception
+		*/
+		try
+		{
+			socket = new Socket("127.0.0.1" , 55555);
+			rdata = new DataInputStream(socket.getInputStream());
+			wdata = new DataOutputStream(socket.getOutputStream());
+			
+			userID = rdata.readInt();
+			
+			Thread thread = new Thread(new datamanager());
+			thread.start();
+		} 
+		catch (Exception e)
+		{
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void updatetoserver()
+	{
+		
+	}
+	
+	static class datamanager implements Runnable
+	{
+		
+		@Override
+		public void run()
+		{
+			/*
+			 * function for immplement multi-thread
+			 * while loop for read update from other client
+			 */
+			
+			while(true)
+			{
+				
+			}
+		}
+	}
 }
--- DoDo.io/src/tw/edu/ncu/softwareengineering/dodoio/Server.java
+++ DoDo.io/src/tw/edu/ncu/softwareengineering/dodoio/Server.java
@@ -1,6 +1,96 @@
 package tw.edu.ncu.softwareengineering.dodoio;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
 public class Server
 {
+	ServerSocket serversocket;
+	static Vector outputvc;
+	int idcount;
 
+	public void startserver()
+	{
+		/*
+		 * Server constructor
+		 * start the server socket
+		 * while loop to listen the connection from client
+		 * handle the socket exception
+		 */
+		
+		try
+		{
+			serversocket = new ServerSocket(55555);
+			outputvc = new Vector();
+			idcount = 0;
+			
+			while(true)
+			{
+				Socket clientsocket = serversocket.accept();
+				if(clientsocket.isConnected())
+				{
+					DataOutputStream wdata = new DataOutputStream(clientsocket.getOutputStream());
+					
+					wdata.writeInt(idcount + 1);
+					wdata.flush();
+					
+					outputvc.add(wdata);
+					++idcount;
+					
+					Thread thread = new Thread(new clientmanager(clientsocket));
+					thread.start();
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+		
+	static class clientmanager implements Runnable
+	{
+		Socket clientsocket;
+		DataInputStream rdata;
+		
+		public clientmanager(Socket client)
+		{
+			/*
+			 * clientmanager constructor
+			 * handle the inputstream exception
+			 */
+
+			try
+			{
+				clientsocket = client;
+				rdata = new DataInputStream(clientsocket.getInputStream());
+			} 
+			catch (Exception e)
+			{
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void run()
+		{
+			/*
+			 * Implement run() for multi-thread
+			 * while loop to read the data from client
+			 * and broacast to every other client
+			 */
+			while(true)
+			{
+				
+			}
+		}
+		
+	}
+	
 }
