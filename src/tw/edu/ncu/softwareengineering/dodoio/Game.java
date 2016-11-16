3171cf2ef4a0095a1b112f9e34dff9eb0cb4a3be
mepaling <dick50414@gmail.com>
2016-11-17 00:33:35 +0800

    新增最初步邏輯 不過99%不能動 也是有一堆問題想問QQ

--- src/tw/edu/ncu/softwareengineering/dodoio/Game.java
+++ src/tw/edu/ncu/softwareengineering/dodoio/Game.java
@@ -2,7 +2,7 @@ package tw.edu.ncu.softwareengineering.dodoio;
 
 public abstract class Game {
 	public int playerID;
-	public Character[] players;
-	abstract void start(ObjectManager myobjmanager);
+	public Character[] playerList;
+	void start(CollideObjectManager myObjManager, String IP, String playerName);
 	abstract int update();
 }
