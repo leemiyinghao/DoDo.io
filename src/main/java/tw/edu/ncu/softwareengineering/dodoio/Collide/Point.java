406ac51f36081b361005a10e7fc0f572dd28e31c
張勝韋 <g84569@yahoo.com.tw>
2016-12-02 00:39:48 +0800

    2016/12/2 12:38 finish collider

---
+++ src/main/java/tw/edu/ncu/softwareengineering/dodoio/Collide/Point.java
@@ -0,0 +1,11 @@
+package tw.edu.ncu.softwareengineering.dodoio.Collide;
+
+public class Point {
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/main/java/tw/edu/ncu/softwareengineering/dodoio/Collide/RectangleCollider.java
+++ src/main/java/tw/edu/ncu/softwareengineering/dodoio/Collide/RectangleCollider.java
@@ -3,34 +3,56 @@ package tw.edu.ncu.softwareengineering.dodoio.Collide;
 import tw.edu.ncu.softwareengineering.dodoio.CollideObject.Position;
 
 public class RectangleCollider implements ICollider {
-    private int leftTopX, leftTopY, rightBottomX, rightBottomY;
-    private Position position;
+    private Position position = new Position(0, 0, 0);
+    private Point[] points = new Point[4];
 
-    public RectangleCollider(Position position, int leftTopX, int leftTopY, int rightBottomX, int rightBottomY) {
-        this.position = position;
-        this.leftTopX = leftTopX;
-        this.leftTopY = leftTopY;
-        this.rightBottomX = rightBottomX;
-        this.rightBottomY = rightBottomY;
-    }
-
-    public int getLeftTopX() {
-        return leftTopX;
-    }
-
-    public int getLeftTopY() {
-        return leftTopY;
-    }
-
-    public int getRightBottomX() {
-        return rightBottomX;
-    }
-
-    public int getRightBottomY() {
-        return rightBottomY;
+    /**Create a rotated RectangleCollider
+     * @param width
+     * @param height
+     * @param position
+     */
+    public RectangleCollider(int width, int height, Position position) {
+        this.position.setPosition(position.getX(), position.getY(), position.getDirection());
+        points[0] = rotatePoint(new Point(position.getX() - width / 2, position.getY() - height / 2), 2 * Math.PI * (1 - position.getDirection()));
+        points[1] = rotatePoint(new Point(position.getX() + width / 2, position.getY() - height / 2), 2 * Math.PI * (1 - position.getDirection()));
+        points[2] = rotatePoint(new Point(position.getX() - width / 2, position.getY() + height / 2), 2 * Math.PI * (1 - position.getDirection()));
+        points[3] = rotatePoint(new Point(position.getX() + width / 2, position.getY() + height / 2), 2 * Math.PI * (1 - position.getDirection()));
     }
 
     public Position getPosition() {
         return position;
+    }
+    public Point[] getPoints() {
+        return points;
+    }
+
+    @Override
+    public void update(Position position) {
+        int XOffset = position.getX() - this.position.getX();
+        int YOffset = position.getY() - this.position.getY();
+        for (Point point : points) {
+            point.x += XOffset;
+            point.y += YOffset;
+        }
+        this.position.setPosition(position.getX(), position.getY(), position.getDirection());
+    }
+
+    /**
+     * Rotate a point with angle
+     * @param p
+     * @param angle
+     * @return a rotated point
+     */
+    private Point rotatePoint(Point p, double angle) {
+        //change origin to current position
+        int x = p.x - position.getX();
+        int y = p.y - position.getY();
+        //multiply rotate matrix
+        x = (int) Math.round(Math.cos(angle) * x - Math.sin(angle) * y);
+        y = (int) Math.round(Math.sin(angle) * x + Math.cos(angle) * y);
+        //change origin to 0,0
+        x += position.getX();
+        y += position.getY();
+        return new Point(x, y);
     }
 }
