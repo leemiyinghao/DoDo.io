fe20d73f1c2d3c552bc38cc189765a8346705657
102206502 <102206502@cc.ncu.edu.tw>
2016-12-07 21:17:41 +0800

    delete the casting method in AttackObject

--- src/main/java/tw/edu/ncu/softwareengineering/dodoio/CollideObject/AttackObject.java
+++ src/main/java/tw/edu/ncu/softwareengineering/dodoio/CollideObject/AttackObject.java
@@ -11,15 +11,5 @@ public abstract class AttackObject extends CollideObject implements Runnable{
 		super(inputID, image, setPosition);
 		// TODO Auto-generated constructor stub
 	}
-
-	/**input the traversal, the object will know where do it move to
-	 * 
-	 * @param setTraversal
-	 */
-	void casting(double setTraversal){
-		int x = (int) ((Math.cos(position.getDirection()*(2*Math.PI)))*setTraversal);
-		int y = (int) ((Math.sin(position.getDirection()*(2*Math.PI)))*setTraversal);
-		position.setPosition(position.getX()+x, position.getY()+y, position.getDirection());
-	}
 	
 }
