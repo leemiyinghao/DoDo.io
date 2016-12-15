d84493cb1e1efc6425deb6661f1e2859f0a2354d
mepaling <dick50414@gmail.com>
2016-12-15 23:19:51 +0800

    move client to outside and create getClient method, the operation of new Client still in the start() method

--- src/main/java/tw/edu/ncu/softwareengineering/dodoio/Game/Game.java
+++ src/main/java/tw/edu/ncu/softwareengineering/dodoio/Game/Game.java
@@ -1,10 +1,15 @@
 package tw.edu.ncu.softwareengineering.dodoio.Game;
 
 import tw.edu.ncu.softwareengineering.dodoio.CollideObject.CollideObjectManager;
+import tw.edu.ncu.softwareengineering.dodoio.Internet.Client;
 
 public abstract class Game {
 	public CollideObjectManager myObjManager;
-	abstract void start(String playerName, CollideObjectManager.ChracterClass profession, int gameMode);
+	Client client;
+	public Client getClient() {
+		return this.client;
+	}
+	abstract void start(String playerName, CollideObjectManager.ChracterClass chracterclass, int gameMode);
 	abstract int update();
 	
 }
