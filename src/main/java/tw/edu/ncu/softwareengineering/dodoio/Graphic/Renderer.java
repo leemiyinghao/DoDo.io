479bac2e2ba291b822d2317a1b75809f8d589404
catLee <leemiyinghao@gmx.com>
2016-12-22 21:42:00 +0800

    Done drawOverlay

--- src/main/java/tw/edu/ncu/softwareengineering/dodoio/Graphic/Renderer.java
+++ src/main/java/tw/edu/ncu/softwareengineering/dodoio/Graphic/Renderer.java
@@ -19,10 +19,10 @@ public class Renderer extends JFrame{
     private GameStat gameStat;
     private Character mainCharacter;
     private float[] windowContainSize = {30f, 9f};
+    private Image overlayBasic;
 
     public Renderer(CollideObjectManager collideObjectManager, Map map){
         this.collideObjectManager = collideObjectManager;
-        this.map = map;
     }
     public void render(int timeOffsetInMs){
         switch(getStat()){
@@ -35,11 +35,27 @@ public class Renderer extends JFrame{
                 break;
         }
     }
+    private Position getRealPositionByPercentage(float x, float y){
+        return new Position((int)(x*this.getWidth()),(int)(y*this.getHeight()), 0);
+    }
     private void drawGameTypeMenu(){
 
     }
     private void drawInGameOverlay(){
-
+        Graphics g = this.getGraphics();
+        g.drawImage(this.overlayBasic, 0, 0, this.getWidth(), this.getHeight(), Color.BLACK, this);
+        //draw HP
+        Position startPos = getRealPositionByPercentage(0.03f, 0.93f);
+        Position duraPos = getRealPositionByPercentage(0.04f, 0.02f);
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(startPos.getX(), startPos.getY(), duraPos.getX(), duraPos.getY());
+        g.setColor(Color.RED);
+        try {
+            g.fillRect(startPos.getX(), startPos.getY(), duraPos.getX()*collideObjectManager.getMyPlayer().getHP(), duraPos.getY());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //TODO: draw RankList, maybe
     }
     private void drawInGameEntriesLayer(){
         for (CollideObject object: collideObjectManager.collideObjectList) {
