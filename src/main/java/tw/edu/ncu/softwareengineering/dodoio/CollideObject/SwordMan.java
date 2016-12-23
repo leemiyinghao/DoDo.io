ea91b4a636ee4364455700546d440cae1f622bd3
102206502 <102206502@cc.ncu.edu.tw>
2016-12-23 11:37:40 +0800

    complete attack request

--- src/main/java/tw/edu/ncu/softwareengineering/dodoio/CollideObject/SwordMan.java
+++ src/main/java/tw/edu/ncu/softwareengineering/dodoio/CollideObject/SwordMan.java
@@ -1,5 +1,7 @@
 package tw.edu.ncu.softwareengineering.dodoio.CollideObject;
 
+import tw.edu.ncu.softwareengineering.dodoio.CollideObject.CollideObjectManager.collideObjecctClass;
+
 
 public class SwordMan extends Character {
 
@@ -10,6 +12,30 @@ public class SwordMan extends Character {
 		damagePoint+=80;
 		healthPoint+=100;
 		recoveryCD = recoveryCD*0.9;
+	}
+	
+	@Override
+	public void attack() {
+		if(attackActive) {
+			attackActive = false;
+			attackCountDown = attackCD;
+			assert(client!=null);
+			client.newattack(collideObjecctClass.Slash, this.ID);
+		}
+		else
+			return;
+	}
+
+	@Override
+	public void skill() {
+		if(skillActive) {
+			skillActive = false;
+			skillCountDown = skillCD;
+			assert(client!=null);
+			client.newattack(collideObjecctClass.SlashBig, this.ID);
+		}
+		else
+			return;
 	}
 
 }
