23bfd986a2df5447148db84349af22c1bf08bcaa
102206502 <102206502@cc.ncu.edu.tw>
2016-12-26 23:55:13 +0800

    remve testSetMainPlayer()

--- src/test/java/tw/edu/ncu/softwareengineering/dodoio/CollideObject/CollideObjectManagerTest.java
+++ src/test/java/tw/edu/ncu/softwareengineering/dodoio/CollideObject/CollideObjectManagerTest.java
@@ -10,9 +10,13 @@ import org.junit.Test;
 
 import tw.edu.ncu.softwareengineering.dodoio.CollideObject.Character.TeamName;
 import tw.edu.ncu.softwareengineering.dodoio.CollideObject.CollideObjectManager.collideObjecctClass;
+import tw.edu.ncu.softwareengineering.dodoio.Game.DeathMatch;
+import tw.edu.ncu.softwareengineering.dodoio.Game.Game;
+import tw.edu.ncu.softwareengineering.dodoio.Game.GameState;
 
 public class CollideObjectManagerTest {
 	CollideObjectManager manager;
+	CollideObjectManager managerClient;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -25,11 +29,13 @@ public class CollideObjectManagerTest {
 	@Before
 	public void setUp() throws Exception {
 		manager = new CollideObjectManager();
+		managerClient = new CollideObjectManager(new DeathMatch());
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		manager = null;
+		managerClient = null;
 	}
 
 	@Test
@@ -46,16 +52,16 @@ public class CollideObjectManagerTest {
 				manager.collideObjectList.get(0).position, (Character) toAddObjectsPlayer);
 	}
 
-	@Test
-	public void testSetMainPlayer() throws Exception {
-		manager.addCharacter(collideObjecctClass.SwordMan, 1, new Position(0, 0, 0), "swordManX", TeamName.teamBlue);
-		SwordMan toAddObjectsPlayer = (SwordMan) manager.collideObjectList.get(0);
-		manager.addAttackObject(collideObjecctClass.SlashBig, 35, 
-				manager.collideObjectList.get(0).position, (Character) toAddObjectsPlayer);
-		manager.addCharacter(collideObjecctClass.Magician, 2, new Position(1, 2, 0), "magicianX", TeamName.teamBlue);
-		manager.setMainPlayer((Character) manager.collideObjectList.get(2));
-		assertNotNull("Main player added", manager.player);
-	}
+//	@Test
+//	public void testSetMainPlayer() throws Exception {
+//		manager.addCharacter(collideObjecctClass.SwordMan, 1, new Position(0, 0, 0), "swordManX", TeamName.teamBlue);
+//		SwordMan toAddObjectsPlayer = (SwordMan) manager.collideObjectList.get(0);
+//		manager.addAttackObject(collideObjecctClass.SlashBig, 35, 
+//				manager.collideObjectList.get(0).position, (Character) toAddObjectsPlayer);
+//		manager.addCharacter(collideObjecctClass.Magician, 2, new Position(1, 2, 0), "magicianX", TeamName.teamBlue);
+//		manager.setMainPlayer((Character) manager.collideObjectList.get(2));
+//		assertNotNull("Main player added", manager.player);
+//	}
 
 	@Test(expected=Exception.class)
 	public void testQueryObjectByID() throws Exception {
@@ -80,16 +86,16 @@ public class CollideObjectManagerTest {
 
 	@Test(expected=Exception.class)
 	public void testGetMyPlayer() throws Exception {
-		manager.addCharacter(collideObjecctClass.SwordMan, 1, new Position(0, 0, 0), "swordManX", TeamName.teamBlue);
-		SwordMan toAddObjectsPlayer = (SwordMan) manager.collideObjectList.get(0);
-		manager.addAttackObject(collideObjecctClass.SlashBig, 35, 
-				manager.collideObjectList.get(0).position, (Character) toAddObjectsPlayer);
-		manager.addCharacter(collideObjecctClass.Magician, 2, new Position(1, 2, 0), "magicianX", TeamName.teamBlue);
-		manager.setMainPlayer((Character) manager.collideObjectList.get(2));
+		managerClient.addCharacter(collideObjecctClass.SwordMan, 1, new Position(0, 0, 0), "swordManX", TeamName.teamBlue);
+		SwordMan toAddObjectsPlayer = (SwordMan) managerClient.collideObjectList.get(0);
+		managerClient.addAttackObject(collideObjecctClass.SlashBig, 35, 
+				managerClient.collideObjectList.get(0).position, (Character) toAddObjectsPlayer);
+		managerClient.addCharacter(collideObjecctClass.Magician, 2, new Position(1, 2, 0), "magicianX", TeamName.teamBlue);
+		managerClient.setMainPlayer((Character) managerClient.collideObjectList.get(2));
 		
-		assertEquals(" get my player", manager.player, manager.getMyPlayer());
-		manager.player = null;
-		manager.getMyPlayer();
+		assertEquals(" get my player", managerClient.player, managerClient.getMyPlayer());
+		managerClient.player = null;
+		managerClient.getMyPlayer();
 	}
 
 }
