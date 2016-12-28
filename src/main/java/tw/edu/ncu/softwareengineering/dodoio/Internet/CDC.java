c6806b423dba95799cedd6b1fd3e1f72e5eba538
genesis <genesis@DESKTOP-4TIND3N>
2016-12-28 22:11:40 +0800

    complete server

--- src/main/java/tw/edu/ncu/softwareengineering/dodoio/Internet/CDC.java
+++ src/main/java/tw/edu/ncu/softwareengineering/dodoio/Internet/CDC.java
@@ -3,20 +3,26 @@ package tw.edu.ncu.softwareengineering.dodoio.Internet;
 
 import tw.edu.ncu.softwareengineering.dodoio.Collide.Bound;
 import tw.edu.ncu.softwareengineering.dodoio.Collide.Utils;
+import tw.edu.ncu.softwareengineering.dodoio.CollideObject.CollideObject;
 import tw.edu.ncu.softwareengineering.dodoio.CollideObject.CollideObjectManager;
+import tw.edu.ncu.softwareengineering.dodoio.CollideObject.CollideObjectManager.collideObjecctClass;
+import tw.edu.ncu.softwareengineering.dodoio.CollideObject.Fertilizer;
 
 public class CDC
 {
 	CollideObjectManager[] collideObjectManager;
 	Utils utils = Utils.getInstance();
-
+	Server server;
 	
-	public CDC()
+	public CDC(Server ss)
 	{
 		// initial collide object manager
 		collideObjectManager = new CollideObjectManager[2];
 		collideObjectManager[0] = new CollideObjectManager();
 		collideObjectManager[1] = new CollideObjectManager();
+		
+		// set server
+		server = ss;
 
 	}
 	
@@ -24,7 +30,30 @@ public class CDC
 	{		
 		// calculate collide
 		utils.CalculateCollide(collideObjectManager[mode], new Bound(0, 0, 7680, 4320));
+		
+		// check which object has change
+		for(CollideObject tohandle : collideObjectManager[mode].collideObjectList)
+		{
+			if(tohandle.getFlag())
+			{
+				// if object is alive, then reset flag
+				if(tohandle.getHP() > 0)
+					tohandle.resetFlag();
+				else
+				{
+					if(tohandle.className == collideObjecctClass.Fertilizer.ordinal())
+						server.mobclients.get(mode).removefertilizer(((Fertilizer)tohandle).size);
+					else if(tohandle.className == collideObjecctClass.Wanderer.ordinal())
+						server.mobclients.get(mode).removewandermonster(tohandle.ID);
+					
+					collideObjectManager[mode].collideObjectList.remove(tohandle);
+				}
 
+				server.broacast_update(mode, tohandle);
+
+			}
+		}
+		
 	}
 	
 }
