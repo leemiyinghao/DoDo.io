62b23c1fcde87ef88d6b1db60a42cf35edabf30a
102206502 <102206502@cc.ncu.edu.tw>
2016-12-30 00:39:21 +0800

    set className of different fertilizer

--- src/main/java/tw/edu/ncu/softwareengineering/dodoio/CollideObject/Fertilizer.java
+++ src/main/java/tw/edu/ncu/softwareengineering/dodoio/CollideObject/Fertilizer.java
@@ -1,6 +1,7 @@
 package tw.edu.ncu.softwareengineering.dodoio.CollideObject;
 
 import tw.edu.ncu.softwareengineering.dodoio.Collide.CircleCollider;
+import tw.edu.ncu.softwareengineering.dodoio.CollideObject.CollideObjectManager.collideObjecctClass;
 
 public class Fertilizer extends Mob {
 	public final int[][] breedTable = {{75, 300, 1}, {50, 600, 2}, {25, 1200, 4}};// breedTable[size][ability]
@@ -15,16 +16,19 @@ public class Fertilizer extends Mob {
 			radius = breedTable[0][0];
 			healthPoint = breedTable[0][1];
 			exp = breedTable[0][2];
+			className = collideObjecctClass.FertilizerBig.ordinal();
 		}
 		if(setSize.equals(Size.medium)){
 			radius = breedTable[1][0];
 			healthPoint = breedTable[1][1];
 			exp = breedTable[1][2];
+			className = collideObjecctClass.FertilizerMedium.ordinal();
 		}
 		if(setSize.equals(Size.small)){
 			radius = breedTable[2][0];
 			healthPoint = breedTable[2][1];
 			exp = breedTable[2][2];
+			className = collideObjecctClass.FertilizerSmall.ordinal();
 		}
 		
 		collider = new CircleCollider(setPosition, radius);
